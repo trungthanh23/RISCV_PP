30479b44ecc175e9f92e397a18084e8c32e7a100
dangvinh29 <164356963+dangvinh29@users.noreply.github.com>
2025-09-01 20:53:40 +0700

    Update control.v

--- hdl/control.v
+++ hdl/control.v
@@ -22,6 +22,7 @@ module control(
         .resultsrc(resultsrc),
         .memwrite(memwrite),
         .alusrc(alusrc),
+        .alusrcU(alusrcU),
         .immsrc(immsrc),
         .regwrite(regwrite),
         .aluop(aluop)
